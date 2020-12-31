bb22a8d5a5f5d87aed0c6d816c73d1b57a7132ef
bperretti <barbaraperretti@hotmail.com>
2020-12-31 12:24:31 -0300

    TotalPurchases auto layout

---
+++ BarbaraPedroKauaTrabalhoFIAP/TotalPurchase/Controller/TotalPurchaseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TotalPurchaseViewController.swift
+//  BarbaraPedroKauaTrabalhoFIAP
+//
+//  Created by Bárbara Perretti on 31/12/20.
+//
+
+import UIKit
+
+class TotalPurchaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
